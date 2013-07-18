a287e2b8bbe0b3c4d4b319158b009d3366053435
Swaagie <info@martijnswaagman.nl>
2013-07-19 01:21:32 +0200

    [major] constructor style for multiple atoms

--- atomic.js
+++ atomic.js
@@ -1,43 +1,90 @@
-(function Atomic(w) {
+(function initAtomic(w) {
   'use strict';
 
-  while(!w.requestAnimationFrame && (vendor = vendors.pop())) {
-    w.requestAnimationFrame = w[vendor + 'RequestAnimationFrame'];
-    w.cancelAnimationFrame = w[vendor + 'CancelAnimationFrame']
-     || w[vendor+'CancelRequestAnimationFrame'];
+  /**
+   * Polyfill for cancel & requestAnimationFrame for smooth and fast animations.
+   *
+   * @api private
+   */
+  (function polyfill() {
+    var expectedTime = 0
+      , vendors = ['ms', 'moz', 'webkit', 'o']
+      , vendor;
+
+    while(!w.requestAnimationFrame && (vendor = vendors.pop())) {
+      w.requestAnimationFrame = w[vendor + 'RequestAnimationFrame'];
+      w.cancelAnimationFrame = w[vendor + 'CancelAnimationFrame']
+       || w[vendor+'CancelRequestAnimationFrame'];
+    }
+
+    if (!w.cancelAnimationFrame) w.cancelAnimationFrame = clearTimeout;
+    if (!w.requestAnimationFrame) {
+      w.requestAnimationFrame = function requestAnimationFrame(callback) {
+        var currentTime = Date.now()
+          , adjustedDelay = 16 - (currentTime - expectedTime)
+          , delay = adjustedDelay > 0 ? adjustedDelay : 0;
+
+        expectedTime = currentTime + delay;
+        return setTimeout(function fps() {
+          callback(expectedTime);
+        }, delay);
+      };
+    }
+  })();
+
+  /**
+   * Constructor of Atomic, called only if the progress bar is in the page.
+   *
+   * @Constructor
+   * @param {Element} atom
+   * @api public
+   */
+  function Atomic(atom) {
+    var electrons = atom.getElementsByTagName('li')
+      , text = atom.getElementsByTagName('input')[0]
+      , canvas = atom.getElementsByTagName('canvas')[0]
+      , size = getComputedStyle(canvas).getPropertyValue('width')
+      , i = electrons.length
+      , n = 100 / i
+      , π = Math.PI
+      , element;
+
+    // Set some properties for the canvas to work with.
+    this.ctx = canvas.getContext('2d');
+    this.color = getComputedStyle(atom).getPropertyValue('color');
+    this.value = +text.value;
+
+    // Some defaults required for the animations
+    this.r = +size.replace('px', '') / 2;
+    this.ψ = π / 50;
+    this.top = π / -2;
+
+    // Attach onChange listeners to each input element
+    text.addEventListener('change', this.listen.bind(this));
+
+    // Set data-n on section to #list items.
+    atom.setAttribute('data-n', i);
+
+    // Add radiobuttons and listen to any clicks on the labels
+    while (i--) {
+      element = document.createElement('input');
+      element.addEventListener('click', this.listen.bind(this));
+
+      atom.insertBefore(this.setAttributes(
+          element
+        , { type: 'radio', id: 'i' + i, name: 's', value: n * i}
+      ), atom.firstChild);
+
+      electrons[i].getElementsByTagName('label')[0].setAttribute('for', 'i' + i);
+    }
+
+    // Set height and width on the canvas.
+    canvas.setAttribute('width', size);
+    canvas.setAttribute('height', size);
+
+    // Start the animation cycle for the first time.
+    this.animate(0, this.value);
   }
-
-  if (!w.requestAnimationFrame) {
-    w.requestAnimationFrame = function requestAnimationFrame(callback) {
-      var currentTime = Date.now()
-        , adjustedDelay = 16 - (currentTime - expectedTime)
-        , delay = adjustedDelay > 0 ? adjustedDelay : 0;
-
-      expectedTime = currentTime + delay;
-      return setTimeout(function fps() {
-        callback(expectedTime);
-      }, delay);
-    };
-  }
-
-  if (!w.cancelAnimationFrame) w.cancelAnimationFrame = clearTimeout;
-
-  var expectedTime = 0
-    , vendors = ['ms', 'moz', 'webkit', 'o']
-    , atom = document.getElementsByClassName('atomic')[0]
-    , electrons = atom.getElementsByTagName('li')
-    , text = atom.getElementsByTagName('input')[0]
-    , canvas = atom.getElementsByTagName('canvas')[0]
-    , ctx = canvas.getContext('2d')
-    , size = getComputedStyle(canvas).getPropertyValue('width').replace('px', '')
-    , color = getComputedStyle(atom).getPropertyValue('color')
-    , value = +text.value
-    , i = electrons.length
-    , n = 100 / i
-    , r = size / 2
-    , π = Math.PI
-    , ψ = 2 * π / 100
-    , vendor;
 
   /**
    * Small helper function to set attributes on created elements.
@@ -47,7 +94,7 @@
    * @returns {Element}
    * @api private
    */
-  Atomic.setAttributes = function setAttributes(element, attributes) {
+  Atomic.prototype.setAttributes = function setAttributes(element, attributes) {
     for(var key in attributes) {
       element.setAttribute(key, attributes[key]);
     }
@@ -63,18 +110,23 @@
    * @returns {Element}
    * @api private
    */
-  Atomic.animate = function init(start, end) {
-    var cw = end > start ? 1 : -1
-      , top = π / -2
-      , λ = 0
+  Atomic.prototype.animate = function animate(start, end) {
+    var self = this
+      , cw = end > start ? 1 : -1
+      , π = Math.PI
       , Δ = π / 40 * cw
       , θ = Δ / 2
-      , α = top + ψ * start
-      , β = top + ψ * end
-      , steps = Math.abs(Math.ceil((β - α) / Δ));
+      , α = this.top + this.ψ * start
+      , β = this.top + this.ψ * end
+      , steps = Math.abs(Math.ceil((β - α) / Δ))
+      , ctx = this.ctx
+      , r = this.r
+      , λ = 0;
 
     /**
-     * Render the slices.
+     * Render each slice
+     *
+     * @api private
      */
     function render() {
       var corr = α !== top || λ ? θ : 0
@@ -88,52 +140,52 @@
       // Start drawing the circle slice.
       ctx.beginPath();
       ctx.globalCompositeOperation = 'destination-' + (~cw ? 'over' : 'out');
-      ctx.fillStyle = ~cw ? color : 'rgba(0, 0, 0, 1)';
+      ctx.fillStyle = ~cw ? self.color : 'rgba(0, 0, 0, 1)';
       ctx.arc(r, r, r, r1, r2, !~cw);
       ctx.lineTo(r, r);
       ctx.closePath();
       ctx.fill();
 
       // stop calling render as soon as steps are done or if equal to end.
-      if (λ++ < steps && r2 !== β) w.requestAnimationFrame(render);
+      if (λ++ < steps && r2 !== β) self.id = w.requestAnimationFrame(render);
     }
 
-    w.requestAnimationFrame(render);
+    this.id = w.requestAnimationFrame(render);
   };
 
-  Atomic.update = function update(end) {
-    if (end === value) return;
-    Atomic.animate(value, value = end);
+  /**
+   * Change the current value of the progress bar
+   *
+   * @param {Number} end
+   * @returns {Atomic} fluent interface
+   * @api public
+   */
+  Atomic.prototype.update = function update(end) {
+    if (end === this.value || end > 100) return this;
+
+    // Stop running animations and start fresh one.
+    w.cancelAnimationFrame(this.id);
+    this.animate(this.value, this.value = end);
+
+    return this;
   };
 
-  Atomic.listen = function listen(e) {
-    Atomic.update(+(e.target || e.srcElement).value);
+  /**
+   * Event listener for any changes on the inputs.
+   *
+   * @param {Event} e
+   * @returns {Atomic} fluent interface
+   * @api private
+   */
+  Atomic.prototype.listen = function listen(e) {
+    this.update(+(e.target || e.srcElement).value);
+
+    return this;
   };
 
-  // Set data-n on `section` to number of `li` elements.
-  atom.setAttribute('data-n', i);
-  canvas.setAttribute('width', size);
-  canvas.setAttribute('height', size);
-
-  // Dynamically add radiobuttons to attach to the labels and listen to any
-  // clicks on the radiobuttons.
-  var element;
-  while (i--) {
-    element = document.createElement('input');
-    element.addEventListener('click', Atomic.listen);
-
-    atom.insertBefore(Atomic.setAttributes(
-        element
-        , { type: 'radio', id: 'i' + i, name: 's', value: n * i}
-    ), atom.firstChild);
-
-    electrons[i].getElementsByTagName('label')[0].setAttribute('for', 'i' + i);
+  // Initialize each progress bar on the page.
+  var atoms = document.getElementsByClassName('atomic');
+  for (var k = 0; k < atoms.length; k++) {
+    initAtomic[k] = new Atomic(atoms[k]);
   }
-
-  // Attach onChange listeners to each input element
-  text.addEventListener('change', Atomic.listen);
-
-  // Start the animation cycle for the first time.
-  Atomic.animate(0, value);
-  w.Atomic = Atomic;
 })(window);
